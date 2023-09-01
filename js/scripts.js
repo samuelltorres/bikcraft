81c77d822b2fd22a2c22a0a485ca986ea9854ee5
samuelltorres <samuelltorres145@gmail.com>
2023-09-01 13:25:06 -0300

    :sparkles: feat: Adicionado dinamicidade aos itens do orçamento

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+// Ativar links do Menu
 const links = document.querySelectorAll('.header-menu a');
 
-function ativarLink(link){
-  const url = location.href
-  const href = link.href
+function ativarLink(link) {
+	const url = location.href;
+	const href = link.href;
 
-  if (url.includes(href)) {
-    link.classList.add('ativo')
-  }
+	if (url.includes(href)) {
+		link.classList.add('ativo');
+	}
 }
 
-links.forEach(ativarLink)
+links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+	const elemento = document.getElementById(parametro)
+	if (elemento) {
+		elemento.checked = true
+	}
+  console.log(elemento)
+}
+
+parametros.forEach(ativarProduto)
