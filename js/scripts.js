aa3fcccc5b8fc0eca106a2fed60254570274cadf
samuelltorres <samuelltorres145@gmail.com>
2023-09-01 14:11:50 -0300

    :sparkles: feat: mudança de imagens na pagina de cada bicicleta

--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,7 @@ function ativarPergunta(event) {
 
 	resposta.classList.toggle('ativa');
 
-	const ativa = resposta.classList.contains('ativa')
+	const ativa = resposta.classList.contains('ativa');
 	pergunta.setAttribute('aria-expanded', ativa);
 
 	console.log(ativa);
@@ -47,3 +47,23 @@ function eventosPerguntas(pergunta) {
 }
 
 perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event) {
+	const img = event.currentTarget;
+	const media = matchMedia('(min-width: 1000px)').matches;
+
+	if (media) {
+		galeriaContainer.prepend(img);
+	}
+}
+
+function eventosGaleria(img) {
+	img.addEventListener('clicl', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
