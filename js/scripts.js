584e4923a028d8a2a2c72d03a2f1a90b034d1c66
samuelltorres <samuelltorres145@gmail.com>
2023-09-01 15:23:35 -0300

    :lipstick: feat: adicionado transiçoes iniciais em todo o site

--- js/scripts.js
+++ js/scripts.js
@@ -50,23 +50,25 @@ perguntas.forEach(eventosPerguntas);
 
 // Galeria de Bicicletas
 
-const galeria = document.querySelectorAll('.bicicleta-imagens img');
-const galeriaContainer = document.querySelector('.bicicleta-imagens');
+// const galeria = document.querySelectorAll('.bicicleta-imagens img');
+// const galeriaContainer = document.querySelector('.bicicleta-imagens');
 
-function trocarImagem(event) {
-	const img = event.currentTarget;
-	const media = matchMedia('(min-width: 1000px)').matches;
+// function trocarImagem(event) {
+// 	const img = event.currentTarget;
+// 	const media = matchMedia('(min-width: 1000px)').matches;
 
-	if (media) {
-		galeriaContainer.prepend(img);
-	}
-}
+// 	if (media) {
+// 		galeriaContainer.prepend(img);
+// 	}
+// }
 
-function eventosGaleria(img) {
-	img.addEventListener('clicl', trocarImagem);
-}
+// function eventosGaleria(img) {
+// 	img.addEventListener('clicl', trocarImagem);
+// }
 
-galeria.forEach(eventosGaleria);
+// galeria.forEach(eventosGaleria);
+
+
 
 // Ativando plugin de animação
 
