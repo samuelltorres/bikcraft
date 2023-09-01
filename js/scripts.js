03e3c92b87c283533c7812a645b88e25ccf1ec1a
samuelltorres <samuelltorres145@gmail.com>
2023-09-01 13:47:21 -0300

    :sparkles: feat: Adicionado dinamicidade às perguntas dos seguros

--- js/scripts.js
+++ js/scripts.js
@@ -16,11 +16,34 @@ links.forEach(ativarLink);
 const parametros = new URLSearchParams(location.search);
 
 function ativarProduto(parametro) {
-	const elemento = document.getElementById(parametro)
+	const elemento = document.getElementById(parametro);
 	if (elemento) {
-		elemento.checked = true
+		elemento.checked = true;
 	}
-  console.log(elemento)
+	console.log(elemento);
 }
 
-parametros.forEach(ativarProduto)
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+	const pergunta = event.currentTarget;
+	const controls = pergunta.getAttribute('aria-controls');
+	const resposta = document.getElementById(controls);
+
+	resposta.classList.toggle('ativa');
+
+	const ativa = resposta.classList.contains('ativa')
+	pergunta.setAttribute('aria-expanded', ativa);
+
+	console.log(ativa);
+}
+
+function eventosPerguntas(pergunta) {
+	pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
